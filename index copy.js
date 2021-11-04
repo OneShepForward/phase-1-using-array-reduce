8ffa817a1eb298a33819cdd52a8365feaf01c27e
Nicholas Shephard <shephardn@gmail.com>
2021-11-04 11:05:02 -0500

    Assignment complete

---
+++ index copy.js
@@ -0,0 +1,11 @@
+const products = [
+    { name: 'Shampoo', price: 4.99 },
+    { name: 'Donuts', price: 7.99 },
+    { name: 'Cookies', price: 6.49 },
+    { name: 'Bath Gel', price: 13.99 }
+  ];
+
+// Code your solution here
+
+const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
+console.log(const doubledAndSummedFromTen = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 10));
